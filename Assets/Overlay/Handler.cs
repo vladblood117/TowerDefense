6c041bc95e51744c6677318c1851d0474b51c1ac
Jerbear420 <jeremie.ac.swartz@gmail.com>
2021-04-20 11:36:47 -0700

    Img and System Update

---
+++ Assets/Overlay/Handler.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Handler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private GameObject BasicBuildingPanel;
+    void Start()
+    {
+
+    }
+
+    public void OpenBasicPanel()
+    {
+
+        BasicBuildingPanel.SetActive(!BasicBuildingPanel.activeSelf);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
